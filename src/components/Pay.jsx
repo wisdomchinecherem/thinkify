d392e449758c3d6c4bc099c189794b53f6ea778e
Wisdom Chinecherem Ekene <wisdom12ekene@gmail.com>
2024-07-11 19:56:25 +0100

    update

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -11,10 +11,10 @@ const Pay = () => {
           <img
             className="cursor-pointer"
             src="../images/Master Card.png"
-            alt=""
+            alt="Master Card"
           />
-          <img className="cursor-pointer" src="../images/Visa.png" alt="" />
-          <img className="cursor-pointer" src="../images/Troy.png" alt="" />
+          <img className="cursor-pointer" src="../images/Visa.png" alt="Visa" />
+          <img className="cursor-pointer" src="../images/Troy.png" alt="Troy" />
         </div>
         <div className="flex items-center gap-[40px] justify-between">
           <div className="leading-[12px]">
@@ -26,15 +26,19 @@ const Pay = () => {
               Enter the 16-digit card number on the card
             </span>
           </div>
-          <div className="flex items-center max-w-[394px] gap-[14px]">
+          <div className="flex items-center w-[394px] gap-[14px]">
             <div className="flex items-center w-full pl-[18px] border-[1.7px] border-solid border-[#9F9F9F] rounded-[3.17px]">
-              <img src="../images/wallet.png" alt="" />
+              <img src="../images/wallet.png" alt="wallet icon" />
               <input
                 type="text"
                 className="outline-none max-w-[394px] border-none indent-[4px] py-[17px]"
               />
             </div>
-            <img className="cursor-pointer" src="../images/check.png" alt="" />
+            <img
+              className="cursor-pointer"
+              src="../images/check.png"
+              alt="check icon"
+            />
           </div>
         </div>
       </div>
@@ -50,9 +54,9 @@ const Pay = () => {
               Enter the name on the card
             </span>
           </div>
-          <div className="flex items-center flex-[1] gap-[14px]">
+          <div className="flex items-center w-[394px] gap-[14px]">
             <div className="flex items-center w-full pl-[18px] border-[1.7px] border-solid border-[#9F9F9F] rounded-[3.17px]">
-              <img src="../images/wallet.png" alt="" />
+              {/* <img src="../images/wallet.png" alt="wallet icon" /> */}
               <input
                 type="text"
                 className="outline-none max-w-[394px] border-none indent-[4px] py-[17px]"
@@ -62,13 +66,45 @@ const Pay = () => {
               className="pointer-events-none"
               style={{ visibility: "hidden" }}
               src="../images/check.png"
-              alt=""
+              alt="check icon"
             />
           </div>
         </div>
       </div>
-      <div>
-        <button>Checkout</button>
+      {/* third */}
+      <div className="flex items-center mt-[14px] justify-between">
+        <div>
+          <p className="font-medium text-[24px]">Expiry date</p>
+          <p className="text-[#6A6A6A]">Enter the expration date of the card</p>
+        </div>
+        <div className="bg-red-600 flex items-center w-[394px]">
+          <div className="flex items-center gap-[8px]">
+            <input
+              type="text"
+              className="w-[36px] h-[35px] border border-solid border-[#9F9F9F] rounded outline-none indent-[3px]"
+            />
+            <span>/</span>
+            <input
+              type="text"
+              className="w-[36px] h-[35px] border border-solid border-[#9F9F9F] rounded outline-none indent-[3px]"
+            />
+          </div>
+          <div className="flex items-center">
+            <div>
+              <p>CVV2</p>
+              <p>Security code</p>
+            </div>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ................. */}
+      <div className="mt-[32px] flex items-center justify-center">
+        <button className="bg-[#08AC9F] border border-white btn-checkout rounded-[30px] text-white py-[16px] px-[87px] text-[18px]">
+          Checkout
+        </button>
       </div>
     </div>
   );
